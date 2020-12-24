18033e3729286433d98c6e5f325e314a0261d3fd
JacksonLoo <304188478@qq.com>
2020-12-24 10:31:25 +0800

    Update package configuration

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -3,7 +3,7 @@ const path = require('path')
 const utils = require('./utils')
 const webpack = require('webpack')
 const config = require('../config')
-const merge = require('webpack-merge')
+const {merge} = require('webpack-merge')
 const baseWebpackConfig = require('./webpack.base.conf')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
@@ -107,7 +107,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     		chunksSortMode: 'dependency'
     	}),
     	// keep module.id stable when vendor modules does not change
-    	new webpack.HashedModuleIdsPlugin(),
+    	new webpack.ids.HashedModuleIdsPlugin(),
     	// enable scope hoisting
     	new webpack.optimize.ModuleConcatenationPlugin(),
     	// split vendor js into its own file
@@ -136,13 +136,20 @@ const webpackConfig = merge(baseWebpackConfig, {
     	// 	minChunks: 3
     	// }),
 		// copy custom static assets
-    	new CopyWebpackPlugin([
-			{
-        		from: path.resolve(__dirname, '../static'),
-        		to: config.build.assetsSubDirectory,
-        		ignore: ['.*']
-    		}
-    	])
+    	
+	// new CopyWebpackPlugin([
+	// 		{
+        // 		from: path.resolve(__dirname, '../static'),
+        // 		to: config.build.assetsSubDirectory,
+        // 		ignore: ['.*']
+    	// 	}
+    	// ]),
+		new CopyWebpackPlugin({
+			patterns:[{
+				from: path.resolve(__dirname,"../static"),
+				to: config.build.assetsSubDirectory
+			}]
+		})
 	]
 })
 
